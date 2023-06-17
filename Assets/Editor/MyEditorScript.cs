5f5809791731277fd3f72735faf00256f73dde70
vibas.behera <vibas.behera095@gmail.com>
2023-06-18 00:53:19 +0530

    Windows Build Script added

--- Assets/Editor/MyEditorScript.cs
+++ Assets/Editor/MyEditorScript.cs
@@ -1,13 +1,13 @@
 using UnityEditor;
 class MyEditorScript
 {
-    [MenuItem("Tools/Android Build")]
-    static void PerformAndroidBuild()
+    [MenuItem("Tools/Windows Build")]
+    static void PerformWindowsBuild()
     {
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
         buildPlayerOptions.scenes = new[] { "Assets/Scenes/Bomberman.unity" };
-        buildPlayerOptions.locationPathName = "AndroidBuild/Bomberman.apk";
-        buildPlayerOptions.target = BuildTarget.Android;
+        buildPlayerOptions.locationPathName = "Win/Bomberman";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
         buildPlayerOptions.options = BuildOptions.None;
         BuildPipeline.BuildPlayer(buildPlayerOptions);
     }
